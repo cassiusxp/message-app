2bda9cadec37d822771d9792eac737cde9d67e79
Cassius <julicapo.288@gmail.com>
2024-06-26 15:46:33 -0300

    Added a home screen, and Firestore support.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,18 +1,77 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:message_app/services/auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _auth = Provider.of<AuthService>(context, listen: false);
     return Scaffold(
-        body: Center(
-      child: TextButton(
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-          child: const Text("saved out")),
-    ));
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          _auth.auth.currentUser!.email!,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.auth.signOut();
+            },
+            icon: Icon(
+              Icons.logout,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+      body: _buildUserListWidget(),
+    );
+  }
+
+  Widget _buildUserListWidget() {
+    final _db = FirebaseFirestore.instance;
+    return StreamBuilder(
+      stream: _db.collection("users").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text("Error"));
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>(
+                (e) => _buildUserTile(e),
+              )
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserTile(QueryDocumentSnapshot doc) {
+    final user = doc.data() as Map<String, dynamic>;
+    if (FirebaseAuth.instance.currentUser!.email != user["email"]) {
+      return ListTile(
+        title: Text(
+          user["email"],
+        ),
+        onTap: () {},
+      );
+    } else {
+      return const SizedBox();
+    }
   }
 }
--- lib/services/auth/firebase_auth.dart
+++ lib/services/auth/firebase_auth.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
-  final FirebaseAuth auth = FirebaseAuth.instance;
+  final firedb = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
   Future<User?> signin(String email, String password) async {
     final UserCredential user =
         await auth.signInWithEmailAndPassword(email: email, password: password);
@@ -12,6 +14,9 @@ class AuthService extends ChangeNotifier {
   Future<User?> signUp(String email, String password) async {
     final UserCredential user = await auth.createUserWithEmailAndPassword(
         email: email, password: password);
+    firedb.collection("users").doc(user.user!.uid).set(
+      {"uid": user.user!.uid, "email": user.user!.email},
+    );
     return user.user;
   }
 }
